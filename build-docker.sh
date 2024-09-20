f648f5280d7216a0226418e36259aafdb44a1c7e
Sebastian Weitzel <sebastian.weitzel@cplace.com>
2024-09-20 15:53:53 +0200

    Update poetry lock file
    Signed-off-by: Sebastian Weitzel <sebastian.weitzel@cplace.com>

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# URL of the chat API used by frontend
-export NEXT_PUBLIC_API_BASE_URL=https://chat-api.example.com
-
 echo ">>> Building frontend"
-docker build -t cplace-docs-chat:latest frontend
+docker build --build-arg='NEXT_PUBLIC_API_BASE_URL=https://chat-api.example.com' -t cplace-docs-chat:latest frontend
 echo ">>> Building backend"
 docker build -t cplace-docs-chat-api:latest .
